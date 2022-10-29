2bcdf858ecdc54e69538b3c62b4616f8e6cca23c
emanuel.furtos <emanuel.furtos@qubiz.ro>
2022-10-30 01:45:16 +0300

    main download functionality

--- server.js
+++ server.js
@@ -1,1 +1,102 @@
+const express = require('express');
+const app = express();
+const path = require('path');
+const fs = require('fs');
+const Enumerable = require('node-enumerable');
 
+const DownloadFolder = 'download';
+const FolderIcon = 'Folder.png';
+const IconsFolder = 'icons';
+
+const IconNames = GetIconNames();
+
+app.get('/download', (req, res) => {
+    SendFile('www/downloader/index.html', res);
+})
+
+app.get('/loader.js', (req, res) => {
+    SendFile('www/downloader/loader.js', res);
+})
+
+app.get('/api/data', (req, res) => {
+    var files = fs.readdirSync(DownloadFolder, { withFileTypes: true });
+
+    var data = {
+        Files: files.map(function (f) {
+            var stats = GetStatsOfFileInDownloadFolder(f);
+
+            return {
+                Name: f.name,
+                Icon: GetIconForFileOrFolder(f),
+                IsDirectory: f.isDirectory(),
+                Size: GetSizeOfFileFromStats(stats),
+                DateCreated: stats.birthtime,
+                LastAccessed: stats.atime,
+                LastModified: stats.mtime,
+                LastMetadataChange: stats.ctime
+            }
+        }),
+    };
+
+    res.send(data);
+})
+
+app.get('/api/icons', (req, res) => {
+    var iconName = req.query.name;
+
+    SendFile(`icons/${iconName}`, res);
+})
+
+app.get('/api/download', (req, res) => {
+    //will change from name to path when supporting navigation in directories
+    var name = req.query.name;
+    
+    DownloadFile(name, res);
+})
+
+var IconCounter = 0;
+
+function GetIconForFileOrFolder(file) {
+    return file.isDirectory() ? FolderIcon : IconNames[(IconCounter++) % IconNames.length];
+}
+
+function GetSizeOfFileFromStats(stats) {
+    var fileSizeInBytes = stats.size;
+    var fileSizeInMegabytes = fileSizeInBytes / (1024 * 1024);
+
+    return {
+        InBytes: fileSizeInBytes,
+        InMegaBytes: fileSizeInMegabytes
+    };
+}
+
+function GetStatsOfFileInDownloadFolder(file) {
+    var stats = fs.statSync(path.join(__dirname, DownloadFolder));
+
+    return stats;
+}
+
+function SendFile(fileName, res) {
+    var options = {
+        root: path.join(__dirname)
+    };
+
+    res.sendFile(fileName, options, function (err) {
+    });
+}
+
+function DownloadFile(fileName, res) {
+    res.download(path.join(__dirname, DownloadFolder, fileName));
+}
+
+function GetIconNames() {
+    var files = fs.readdirSync(path.join(__dirname, IconsFolder), { withFileTypes: true });
+
+    return Enumerable.from(files).where(f => f.name !== FolderIcon && !f.isDirectory() && (f.name.endsWith('.jpg') || f.name.endsWith('.png'))).select(s => s.name).toArray();
+}
+
+function GetRandomNumber(upperLimit) {
+    return Math.floor(Math.random() * upperLimit);;
+}
+
+app.listen(9000);
---
+++ www/downloader/loader.js
@@ -0,0 +1,63 @@
+fetch('api/data').then(d => d.json()).then(data => {
+    console.log(data)
+
+    var holder = GetFileHolder();
+
+    for (var file of data.Files) {
+        holder.appendChild(CreateElement(file));
+    }
+});
+
+function CreateElement(fileProperties) {
+    var elementContainer = document.createElement('div');
+    elementContainer.style.textAlign = 'center';
+    elementContainer.style.float = 'left';
+    elementContainer.style.margin = '10px'
+    elementContainer.style.padding = '5px'
+    elementContainer.style.border = 'solid black'
+
+    elementContainer.className = 'element-container';
+
+    var icon = document.createElement('img');
+    icon.src = `/api/icons?name=${fileProperties.Icon}`
+    icon.style.maxWidth = '100%';
+    icon.style.maxHeight = '100%';
+
+    var title = document.createElement('h3');
+    title.innerHTML = fileProperties.Name;
+
+
+
+
+    elementContainer.appendChild(icon);
+    elementContainer.appendChild(title);
+
+    if (!fileProperties.IsDirectory) {
+        var downloadButton = document.createElement('button')
+        downloadButton.className = 'download-button regular-text';
+        downloadButton.innerHTML = 'Download';
+        downloadButton.onclick = () => {
+            DownloadFile(fileProperties.Name);
+        };
+        elementContainer.appendChild(downloadButton);
+    } else {
+        var stepIntoButton = document.createElement('button')
+        stepIntoButton.className = 'stepinto-button regular-text';
+        stepIntoButton.innerHTML = 'Step into';
+
+        elementContainer.appendChild(stepIntoButton);
+    }
+
+
+    return elementContainer;
+}
+
+function DownloadFile(fileName) {
+    //will change from name to path when supporting navigation in directories
+    //fetch(`/api/download?name=${fileName}`)
+    window.location.href = `/api/download?name=${fileName}`;
+}
+
+function GetFileHolder() {
+    return document.getElementById('fileHolderId');
+}
